53819d1bb7ce5974d36e2c8b80cb068dd8c2ce64
furuyashikiiori <s2322057@stu.musashino-u.ac.jp>
2024-05-22 18:37:25 +0900

    SNSページ作成

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import HomePage from "./conponents/components/HomePage";
 import BlogPage from "./conponents/components/BlogPage";
 import Header from "./conponents/components/Header";
+import SnsPage from "./conponents/components/SnsPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/blog" element={<BlogPage />} />
+          <Route path="/sns" element={<SnsPage />} />
         </Routes>
       </div>
     </Router>
---
+++ src/conponents/components/SnsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SnsPage() {
+  return (
+    <div className="container">
+      <h1>SNS</h1>
+      <p>ここにSNSかくか？？</p>
+    </div>
+  );
+}
+
+export default SnsPage;
