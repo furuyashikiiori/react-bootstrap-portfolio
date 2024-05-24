1be875ebe36f763e2ff05648009c5303ba665fc7
furuyashikiiori <s2322057@stu.musashino-u.ac.jp>
2024-05-24 14:21:06 +0900

    作った作品なんてないよ

--- src/conponents/components/HomePage.js
+++ src/conponents/components/HomePage.js
@@ -25,44 +25,7 @@ function HomePage() {
               私が作った作品一覧です
             </h3>
           </div>
-          <div class="row text-center">
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 class="my-3">ECサイト</h4>
-              <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
-            </div>
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 class="my-3">レスポンシブサイト</h4>
-              <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
-            </div>
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 class="my-3">ウェブセキュリティ</h4>
-              <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
-            </div>
-          </div>
+          <div>ないわ！！！</div>
         </div>
       </section>
 
@@ -73,17 +36,10 @@ function HomePage() {
             <div class="col-md-4 services">
               <img src={reactImage} />
               <h4>React</h4>
-              <p>Reactがつかえます</p>
             </div>
             <div class="col-md-4 services">
               <img src={jsImage} />
               <h4>HTML/CSS</h4>
-              <p>HTML/CSSがつかえます</p>
-            </div>
-            <div class="col-md-4 services">
-              <img src={firebaseImage} />
-              <h4>Firebase</h4>
-              <p>Firebaseがつかえます</p>
             </div>
           </div>
           <button type="button" class="btn btn-primary">
